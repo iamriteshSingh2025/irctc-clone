ee62710409b2f02fb8d51b7fbb5ef01ad338eae6
Ritesh Singh <rs3047520@gmail.com>
2025-03-19 22:29:42 +0530

    changes in pnr fine

--- vite-project/src/components/Auth/Pnrstatus.jsx
+++ vite-project/src/components/Auth/Pnrstatus.jsx
@@ -4,7 +4,7 @@ import { Link, Navigate, useNavigate } from "react-router";
 const Pnrstatus = () => {
   return (
     <div className=' w-full h-30 border '>
-      <h1>hdcggiud</h1>
+      <h1>pnr status</h1>
     </div>
   )
 }
